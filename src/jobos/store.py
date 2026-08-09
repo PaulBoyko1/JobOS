f0704e5b5b6cad974a3c658b984cbdd437a6de8a
Paul Boyko <Paulboyko20@gmail.com>
2026-08-08 22:31:43 -0700

    style: keep transaction handling within normal exceptions

--- src/jobos/store.py
+++ src/jobos/store.py
@@ -218,7 +218,7 @@ class ApplicationStore:
         try:
             yield connection
             connection.commit()
-        except BaseException:
+        except Exception:
             connection.rollback()
             raise
         finally:
